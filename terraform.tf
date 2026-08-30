bd8bdd6569892dcb11babd97bbdcda5fa6b1ca3f
Francesco Di Sales <f.disales@hotmail.com>
2026-08-30 18:26:47 +0200

    Initialize Terraform with Git and Docker providers
    Add Terraform configuration for Git and Docker providers with resources for cloning a Git repository and managing Docker Compose.

---
+++ terraform.tf
@@ -0,0 +1,40 @@
+terraform {
+  required_providers {
+    git = {
+      source  = "metio/git"
+      version = "2026.8.21"
+    }
+
+ docker = {
+      source  = "kreuzwerker/docker"
+      version = "4.5.0"
+    }
+
+  }
+}
+
+provider "git" {
+  # Configuration options
+}
+
+provider "docker" {
+
+}
+
+
+resource "git_clone" "clone" {
+  directory      = "./dwva"
+  url            = "https://github.com/digininja/DVWA"
+  reference_name = "master"
+}
+
+resource "docker_compose" "composedvwa" {
+  project_name = "composedwva"
+
+  config_paths = [
+    "${path.module}/dwva/compose.yml",
+  ]
+}
+
+
+
